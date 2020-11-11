9384c51bf9a8f3a36e7d3cbe43acb825a7df4cf3
TFCarlos <carlos@tufactoria.com>
2020-11-11 15:09:05 +0100

    1 file modified

--- script.js
+++ script.js
@@ -4,19 +4,18 @@ const changeBackgroundButton = document.getElementById(
   "changeBackgroundButton"
 );
 
-changeBackgroundButton.addEventListener("click", function() {
-  this.style.backgroundColor = getRandomColor();
-});
+changeBackgroundButton.addEventListener(
+  "click",
+  () => (changeBackgroundButton.style.backgroundColor = getRandomColor())
+);
 
-function getRandomColor() {
+let getRandomColor = () => {
   const letters = getLetters();
-  let color = '#';
+  let color = "#";
   for (var i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
-}
+};
 
-function getLetters(){
-  return '0123456789ABCDEF';
-}
+let getLetters = () => "0123456789ABCDEF";
